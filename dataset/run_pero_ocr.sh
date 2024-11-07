2597bc82ec64add8f2d014363f84b9cefa4680ec
vlachvojta <vojtik.vlach@seznam.cz>
2024-11-07 15:05:46 +0100

    Add option to run PERO-OCR + run it on example data

---
+++ dataset/run_pero_ocr.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Author: Vojtěch Vlach
+# Brief: Run PERO-OCR inference on specified folder of images
+# Date: 10.10.2024
+# Arguments:
+#   $1: path to folder of images to infer
+# Usage: ./run_pero_ocr.sh /path/to/images
+# Description: Saves resulting data (page xmls and renders) in folders next to the images (for example /path/to/images -> /path/to/images-xml, /path/to/images-render)
+
+# return $2 if $1 is empty (return default value if argument is not provided)
+get_arg_or_default() {
+    if ! [ -z "$1" ]; then
+        echo $1
+    else
+        echo $2
+    fi
+}
+
+IMAGES=$(get_arg_or_default $1 ./)
+if ! [ -d "$IMAGES" ]; then
+    echo "Directory $IMAGES does not exist. Provide a valid path as the first argument."
+    exit 1
+fi
+
+RENDER_PATH=${IMAGES}/render
+RENDER_LINE_PATH=${IMAGES}/render_line
+XML_PATH=${IMAGES}/xml
+
+source ~/.env_pero/bin/activate
+
+# Generate xmls
+# python $PERO_OCR/user_scripts/parse_folder.py -c pipeline_config_double_layout_parser_and_engines_cpu.ini -i images --output-render-path output_render --output-line-path output_line/ --output-xml-path xmls --output-alto-path xmls --output-logit-path logits --device cpu  # CPU
+
+files_in_image_dir=$(ls -p -1 $IMAGES | grep -v / | wc -l)
+echo "Reading ${files_in_image_dir} image files from ${IMAGES}":
+echo ""
+
+python $PERO_OCR/user_scripts/parse_folder.py \
+    -c pipeline_layout_sort_ocr.ini \
+    -i $IMAGES \
+    --output-xml-path $XML_PATH \
+    --output-line-path $RENDER_LINE_PATH \
+    --output-render-path $RENDER_PATH \
+    --output-render-category \
+
+# print info about input and output files
+files_exported=$(ls -p -1 $XML_PATH/*.xml | wc -l)
+
+if [ -d "$XML_PATH" ]; then
+    echo -e "${GREEN}Exported ${files_exported} files from ${files_in_image_dir} images${ENDCOLOR}"
+else
+    echo -e "${RED}No XML files were exported${ENDCOLOR}"
+fi
+
---
+++ dataset/table_detection_to_table_structure_prediction.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script is here to run scripts in the correct order. And give them the correct arguments.
+
+
+# return $2 if $1 is empty (return default value if argument is not provided)
+get_arg_or_default() {
+    if ! [ -z "$1" ]; then
+        echo $1
+    else
+        echo $2
+    fi
+}
+
+WORKDIR=$(get_arg_or_default $1 "example_data")
+
+
+# 0 - input: images of pages + table annotations
+# 1 - cutout table annotations
+CUT_TABLE_DIR=$WORKDIR/1_cut_tables
+python cut_annotations.py -i $WORKDIR/images/ -l $WORKDIR/0_annotated_table_detection.json -o $CUT_TABLE_DIR -f "Table"
+
+# OCR Page-XML of table annotations
+./run_pero_ocr.sh $CUT_TABLE_DIR
+
+# 2 - create tasks for tables with OCR TextLines as predictions 
+python ocr_to_tasks.py -i $CUT_TABLE_DIR -x $CUT_TABLE_DIR/xml -o $WORKDIR/2_cell_detection_tasks
+
