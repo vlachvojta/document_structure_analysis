03a2349dc6236d1ce3767622cc7c613f7edaebd8
vlachvojta <vojtik.vlach@seznam.cz>
2025-04-14 01:32:17 +0200

    put stuff into engine methods

--- table_transformer_baseline/table_structure_recognition_engine.py
+++ table_transformer_baseline/table_structure_recognition_engine.py
@@ -15,97 +15,76 @@ from pubtables_1m.voc_xml import VocObject, VocLayout
 class TableStructureRecognitionEngine:
     """Use microsoft table structure recognition model to recognize table structure in images."""
 
-    def __init__(self, model_name: str, device: str = "cuda"):
-        self.model_name = model_name
+    def __init__(self, model_name: str = "microsoft/table-transformer-structure-recognition",
+                 device: str = "cuda"):
         self.device = device
-        self.model = self.load_model()
+        self.feature_extractor = DetrImageProcessor()
+        self.tsr_model = TableTransformerForObjectDetection.from_pretrained(model_name)
+
+        self.id2label = self.tsr_model.config.id2label
 
     def __call__(self, image) -> list[list[float]]:
-        # see guide in the second part of
-        # Using_Table_Transformer_for_table_detection_and_table_structure_recognition.ipynb
+        encoding = self.feature_extractor(image, return_tensors="pt")
 
-        # Perform table structure recognition on the input image
-        # should return whatever is the output of the model
-        # scores, labels, bboxes = self.model(image)
-        # return scores, labels, bboxes
-        pass
+        with torch.no_grad():
+            outputs = self.tsr_model(**encoding)
 
-    def load_model(self):
-        # Load the model from the specified path or URL
-        pass
+        target_sizes = [image.size[::-1]]
+        results = self.feature_extractor.post_process_object_detection(outputs, threshold=0.6, target_sizes=target_sizes)[0]
+        return results
+    
+    def call_result_to_voc_objects(self, results: dict) -> list[VocObject]:
+        detected_objects = []
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            box = [round(i, 2) for i in box.tolist()]
+            xmin, ymin, xmax, ymax = box
+            detected_object = VocObject(category=self.id2label[label.item()],
+                                        xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax,
+                                        confidence=score.item())
+            detected_objects.append(detected_object)
 
-    def visualize(self, image, detections):
-        # Visualize the detected tables on the image
-        # render detections to the image
-        pass
+        return detected_objects
 
-    def recognize_voc_layout(self, image) -> VocLayout:
-        # Recognize the table structure in the image and return a VOC layout
+    def call_result_to_voc_layout(self, image, results: dict, image_name: str) -> VocLayout:
+        detected_objects = self.call_result_to_voc_objects(results)
 
-        # results = self(image)  # get table structure recognition results
-        # Process the results to create a VOC layout
-        # return voc_layout
-        pass
+        height, width = image.size
 
-    # def recognize_page_layout(self, image) -> PageLayout:
-    #     # Recognize the page layout in the image and return a page layout
+        voc_layout = VocLayout(objects=detected_objects,
+                               width=width, height=height, depth=3,
+                               table_id=os.path.splitext(os.path.basename(image_name))[0])
+        return voc_layout
 
-    #     # results = self(image)  # get table structure recognition results
-    #     # Process the results to create a page layout
-    #     # return page_layout
-    #     pass
+    def plot_categories(self, image, results, categories: list[str]) -> np.ndarray:
+        if isinstance(image, Image.Image):
+            image = np.array(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-def plot_certain_categories(image: np.ndarray, results: dict, categories: list[str], id2label: dict) -> np.ndarray:
-    # if image is a PIL image, convert to numpy array
-    if isinstance(image, Image.Image):
-        image = np.array(image)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            label = self.id2label[label.item()]
+            if categories and label not in categories:
+                continue
 
-    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-        label = id2label[label.item()]
-        if label not in categories:
-            continue
+            box = [round(i, 2) for i in box.tolist()]
+            xmin, ymin, xmax, ymax = box
+            cv2.rectangle(image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)
+            cv2.putText(image, f"{label}: {score:.2f}", (int(xmin), int(ymin) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        box = [round(i, 2) for i in box.tolist()]
-        xmin, ymin, xmax, ymax = box
-        cv2.rectangle(image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)
-        cv2.putText(image, f"{label}: {score:.2f}", (int(xmin), int(ymin) - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    return image
+        return image
 
 
 def main():
-
     file_path = hf_hub_download(repo_id="nielsr/example-pdf", repo_type="dataset", filename="example_table.png")
     image = Image.open(file_path).convert("RGB")
 
     width, height = image.size
     image.resize((int(width*0.5), int(height*0.5)))
-    # save image
-    image.save("example_data/tables/example_table.png")
 
+    tsr_model = TableStructureRecognitionEngine()
+    results = tsr_model(image)
 
-    feature_extractor = DetrImageProcessor()
-
-    encoding = feature_extractor(image, return_tensors="pt")
-
-    model = TableTransformerForObjectDetection.from_pretrained("microsoft/table-transformer-structure-recognition")
-
-    with torch.no_grad():
-        outputs = model(**encoding)
-
-    target_sizes = [image.size[::-1]]
-    results = feature_extractor.post_process_object_detection(outputs, threshold=0.6, target_sizes=target_sizes)[0]
-
-    detected_objects = []
-    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-        box = [round(i, 2) for i in box.tolist()]
-        xmin, ymin, xmax, ymax = box
-        detected_object = VocObject(category=model.config.id2label[label.item()],
-                                    xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax,
-                                    confidence=score.item())
-        detected_objects.append(detected_object)
+    detected_objects = tsr_model.call_result_to_voc_objects(results)
 
     print(f'detected objects:')
     for detected_object in detected_objects:
@@ -114,12 +93,15 @@ def main():
     output_render_folder = "example_data/tables_tsr_renders/"
     os.makedirs(output_render_folder, exist_ok=True)
 
-    image_columns = plot_certain_categories(np.array(image), results, ["table column"], model.config.id2label)
+    image_columns = tsr_model.plot_categories(np.array(image), results, ["table column"])
     cv2.imwrite(os.path.join(output_render_folder, "columns.png"), image_columns)
-    image_rows = plot_certain_categories(np.array(image), results, ["table row", "table projected row header"], model.config.id2label)
+    image_rows = tsr_model.plot_categories(np.array(image), results, ["table row", "table projected row header"])
     cv2.imwrite(os.path.join(output_render_folder, "rows.png"), image_rows)
-    image_other = plot_certain_categories(np.array(image), results, ["table spanning cell", "table column header", "table"], model.config.id2label)
+    image_other = tsr_model.plot_categories(np.array(image), results, ["table spanning cell", "table column header", "table"])
     cv2.imwrite(os.path.join(output_render_folder, "other.png"), image_other)
+
+    voc_layout = tsr_model.call_result_to_voc_layout(image, results, "example_table.png")
+    print(f'voc layout: {voc_layout}')
 
 
 if __name__ == "__main__":
